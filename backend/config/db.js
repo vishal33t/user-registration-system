fb873808349cf0c9ad39d8ab3450572d02a187a4
vishal33t <rajputvishal33t@gmail.com>
2026-06-30 22:57:53 +0530

    some attention required for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,26 +1,28 @@
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
 
-const client = new MongoClient(process.env.MONGODB_URI);
-
-let database;
+let client;
+let db;
 
 async function connectDB() {
-    try {
-        await client.connect();
 
-        database = client.db(process.env.DB_NAME);
-
-        console.log("✅ MongoDB Connected Successfully");
-    } catch (error) {
-        console.error("❌ Database Connection Failed");
-        console.error(error);
-        process.exit(1);
+    if (db) {
+        return db;
     }
+
+    client = new MongoClient(process.env.MONGODB_URI);
+
+    await client.connect();
+
+    db = client.db(process.env.DB_NAME);
+
+    console.log("MongoDB Connected");
+
+    return db;
 }
 
 function getDB() {
-    return database;
+    return db;
 }
 
 module.exports = {
